c15d3bad5287c1b1d8e6d9749bda68e2cdc1bd54
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2023-03-26 21:59:50 +0100

    added section to create new post and fixed tthe searc botton

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,15 +1,44 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for, redirect
 from models import Post, Tag 
+from .form import PostForm 
+
+from app import db
 
 posts = Blueprint('Posts', __name__, template_folder='templates')
 
+@posts.route('/create', methods=["GET","POST"])
+def post_create():
+    form = PostForm()
+    if request.method =="POST":
+        title = request.form.get("title")
+        body= request.form.get('body')
+        
+        
+        try:
+            post = Post(title=title, body=body)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print("an error occured, please try  again.")
+        return redirect(url_for('Posts.post_details', slug=post.slug))
+    else:
+        return render_template('posts/post_create.html', form= form)
+
+
+
+
 @posts.route('/')
 def posts_list():
-    posts = Post.query.all()
+    
+    q =request.args.get('q')
+    if q:
+        posts =Post.query.filter(Post.title.contains(q)| Post.body.contains(q))
+    else:
+        posts = Post.query.order_by(Post.date_created.desc())
     return render_template('posts/post.html', posts = posts)
 
 
-@posts.route('/<slug>')
+@posts.route('/<slug>') 
 def post_details(slug):
     post = Post.query.filter(Post.slug == slug).first()
     return render_template('posts/post_detail.html', post = post) 
@@ -17,4 +46,4 @@ def post_details(slug):
 @posts.route('/tags/<slug>')
 def tag_detail(slug):
     tag = Tag.query.filter(Tag.slug == slug).first()
-    return render_template('posts/tag_details.html', tag = tag)   
+    return render_template('posts/tag_details .html', tag = tag)   
